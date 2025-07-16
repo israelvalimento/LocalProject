75daaf6af6e60c3661d6aee6e6655215e05ad685
israelvalimento <icvalimento@gmail.com>
2025-07-16 20:29:37 +0800

    first-commit

---
+++ tuper-ai/src/App.jsx
@@ -0,0 +1,14 @@
+import { Tuperai } from "./pages/tuper-ai";
+import { Modal } from "./components/Modal";
+function App() {
+  return (
+    <>
+      <Modal></Modal>
+      <main>
+        <Tuperai></Tuperai>
+      </main>
+    </>
+  );
+}
+
+export default App;
